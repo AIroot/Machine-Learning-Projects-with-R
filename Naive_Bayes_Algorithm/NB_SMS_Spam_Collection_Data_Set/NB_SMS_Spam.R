cb8a4ecdb75512f667585a477f6d976c2eb6e7b1
AI1988 <aijpathirana@gmail.com>
2017-05-28 15:22:55 -0500

    The type element converts from character vector to factor

--- Naive_Bayes_Algorithm/NB_SMS_Spam_Collection_Data_Set/NB_SMS_Spam.R
+++ Naive_Bayes_Algorithm/NB_SMS_Spam_Collection_Data_Set/NB_SMS_Spam.R
@@ -8,8 +8,10 @@ sms_data <- read.csv("SMSSpamCollection", sep="\t", header = FALSE, quote="", st
 # # Assign attribute information
 colnames(sms_data) <- c("type", "text")
 
-# Loading in the data, keeping only the first 2 columns
-sms_data <- sms_data[,1:2]
+# The type element is currently a character vector. 
+# Convert it into a factor.
+sms_data$type <- factor(sms_data$type)
+
 # Displays description of each variable 
 str(sms_data)
 head(sms_data)
