3587cf338a8b0cc34f11ba030b85e4a403fd9fd1
AI1988 <aijpathirana@gmail.com>
2017-05-28 14:55:47 -0500

    Change the data resource

--- Naive_Bayes_Algorithm/NB_SMS_Spam_Collection_Data_Set/NB_SMS_Spam.R
+++ Naive_Bayes_Algorithm/NB_SMS_Spam_Collection_Data_Set/NB_SMS_Spam.R
@@ -3,7 +3,7 @@
 
 # Exploring and preparing the data 
 # Read the CSV file into a data frame titled sms_data
-sms_data <- read.csv("sms_spam.csv", header = TRUE)
+sms_data <- read.csv("SMSSpamCollection", sep="\t", header = FALSE, quote="", stringsAsFactors=FALSE)
 
 # # Assign attribute information
 colnames(sms_data) <- c("type", "text")
@@ -14,3 +14,10 @@ sms_data <- sms_data[,1:2]
 str(sms_data)
 head(sms_data)
 table(sms_data$type)
+
+# Data preparation - cleaning and standrdizing text data
+# The tm package can be installed via the install.packages("tm") and 
+# loaded with the library(tm) command. 
+library(tm)
+sms_corpus <- VCorpus(VectorSource(sms_data$text))
+print(sms_corpus)
