0f2a3a4460544f122ff1d94839d5f0983cd31012
AI1988 <aijpathirana@gmail.com>
2017-05-31 10:52:44 -0500

    Updated

--- Naive_Bayes_Algorithm/NB_SMS_Spam_Collection_Data_Set/NB_SMS_Spam.R
+++ Naive_Bayes_Algorithm/NB_SMS_Spam_Collection_Data_Set/NB_SMS_Spam.R
@@ -1,5 +1,12 @@
 # Naive Bayes
 # Source of data set : UCI Repository - http://archive.ics.uci.edu/ml/datasets/SMS+Spam+Collection
+# Reference for data source (
+# @misc{Lichman:2013 ,
+# author = "M. Lichman",
+# year = "2013",
+# title = "{UCI} Machine Learning Repository",
+# url = "http://archive.ics.uci.edu/ml",
+# institution = "University of California, Irvine, School of Information and Computer Sciences" })
 
 # Exploring and preparing the data 
 # Read the CSV file into a data frame titled sms_data
