c5c3029ea44c4e8e144d7f04d5392a750ee8c7d0
AI1988 <aijpathirana@gmail.com>
2017-05-31 11:55:32 -0500

    Add data reference

--- k_Nearest_Neighbors_Algorithm/knn_UCI_Heart_Disease_Data_Set/HeartDiseaseAnalysis.R
+++ k_Nearest_Neighbors_Algorithm/knn_UCI_Heart_Disease_Data_Set/HeartDiseaseAnalysis.R
@@ -2,6 +2,13 @@
 
 # K Nearest Neighbour (KNN) 
 # Source of Data Set:-  UCI Repository- Heart Disease Data (https://archive.ics.uci.edu/ml/datasets/Heart+Disease)
+# Reference for data source (
+# @misc{Lichman:2013 ,
+# author = "M. Lichman",
+# year = "2013",
+# title = "{UCI} Machine Learning Repository",
+# url = "http://archive.ics.uci.edu/ml",
+# institution = "University of California, Irvine, School of Information and Computer Sciences" })
 
 
 # Step 01 : Collecting data
