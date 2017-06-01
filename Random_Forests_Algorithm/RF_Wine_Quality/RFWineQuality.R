6b01a4bc1a3c9544b1f5ea60fc60b95ff74460c2
AI1988 <aijpathirana@gmail.com>
2017-06-01 17:17:19 -0500

    Create project to predict wine quality using random forests

---
+++ Random_Forests_Algorithm/RF_Wine_Quality/RFWineQuality.R
@@ -0,0 +1,7 @@
+# Reference for data source (
+# @misc{Lichman:2013 ,
+# author = "M. Lichman",
+# year = "2013",
+# title = "{UCI} Machine Learning Repository",
+# url = "http://archive.ics.uci.edu/ml",
+# institution = "University of California, Irvine, School of Information and Computer Sciences" })
