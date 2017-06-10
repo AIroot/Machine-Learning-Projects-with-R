5dacb7975325db533cf4f7406448195304c5795c
AI1988 <aijpathirana@gmail.com>
2017-06-10 12:27:50 -0500

    Read xls format using r data frame

--- A_Neural_Network_Algorithm/ANN_Concrete_Compressive_Strengt_Data_Model/ANN_Concrete_Compressive_Strengt_Data_Model.R
+++ A_Neural_Network_Algorithm/ANN_Concrete_Compressive_Strengt_Data_Model/ANN_Concrete_Compressive_Strengt_Data_Model.R
@@ -5,3 +5,42 @@
 # title = "{UCI} Machine Learning Repository",
 # url = "http://archive.ics.uci.edu/ml",
 # institution = "University of California, Irvine, School of Information and Computer Sciences" })
+
+# Neural Networks
+# Source of Data Set:- UCI Repository - (https://archive.ics.uci.edu/ml/datasets/Concrete+Compressive+Strength)
+# Exploring and preparing the data
+# Step 2: Exploring and preparing the data
+# Read the csv file into a data frame titled ConcreteData.
+library(xlsx)
+ConcreteData <- read.xlsx("Concrete_Data.xls",1)
+colnames(ConcreteData) <- c("Cement_comp", "BF_Slag", "Fly_Ash", "Water", "Superplasticizer", "Coarse_Aggregate", "Fine_Aggregate", "Age_Day", "Compressive_Strength")
+str(ConcreteData)
+
+# Transformation - normalizing numeric data
+
+normalize <- function(x) {
+	return ((x - min(x)) / (max(x) - min(x)))
+}
+
+# Apply normalize function to each list elememt. 
+ConcreteData_n <- as.data.frame(lapply(ConcreteData, normalize))
+
+summary(ConcreteData_n$Compressive_Strength)
+summary(ConcreteData$Compressive_Strength)
+
+# Data preparation - creating random training and test datasets
+# Create random sample
+# Divide the data into a training set and a test set randomly with ratio 75:25
+
+set.seed(123)
+train_sample <- sample(nrow(ConcreteData_n), 0.75 * nrow(ConcreteData_n))
+ConcreteData_train <- ConcreteData_n[train_sample, ]
+ConcreteData_test <- ConcreteData_n[-train_sample, ]
+
+# Train model
+
+# # NeuralNet 
+# # # Training a model on the data
+# # # The neuralnet package can be installed via the install.packages("neuralnet") and 
+# # # loaded with the library(neuralnet) command.
+library(neuralnet)
