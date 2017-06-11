5a17c17a582f27e2160e05cddc6af3780cea1eb5
AI1988 <aijpathirana@gmail.com>
2017-06-10 21:34:28 -0500

    Collect data and Assigning attributes

--- Support_Vector_Machine/SVM_Letter_Recognition_Model/SVM_Letter_Recognition_Model.R
+++ Support_Vector_Machine/SVM_Letter_Recognition_Model/SVM_Letter_Recognition_Model.R
@@ -5,3 +5,23 @@
 # title = "{UCI} Machine Learning Repository",
 # url = "http://archive.ics.uci.edu/ml",
 # institution = "University of California, Irvine, School of Information and Computer Sciences" })
+
+# Support Vector Machine
+# Source of Data Set:-  UCI Repository- Letter Recognition Data (https://archive.ics.uci.edu/ml/datasets/letter+recognition)
+
+# # Collecting data
+# # Download data from UCI repo
+# LetterDataUrl <- "https://archive.ics.uci.edu/ml/machine-learning-databases/letter-recognition/letter-recognition.data"
+
+# # Read the url html file into a data frame titled LetterData.
+# LetterData <- read.csv(LetterDataUrl, header=FALSE)
+
+# # Assigning attributes information 
+# colnames(LetterData) <- c("lettr", "x_box", "y_box", "width", "high", "onpix", "x_bar", "y_bar", "x2bar", "y2bar", "xybar", "x2ybr", "xy2br", "x_ege", "xegvy", "y_ege", "yegvx")
+
+# # Write a CSV file from LetterData
+# LetterData <- write.csv(LetterData, file = "LetterData.csv", row.names = FALSE)
+
+LetterData <- read.csv("LetterData.csv", header= TRUE)
+
+str(LetterData)
