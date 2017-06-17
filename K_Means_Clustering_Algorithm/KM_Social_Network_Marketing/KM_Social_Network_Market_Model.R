85eb155d7dbb594a41a6f640f11898253371972f
AI1988 <aijpathirana@gmail.com>
2017-06-17 15:32:13 -0500

    Collecting data

---
+++ K_Means_Clustering_Algorithm/KM_Social_Network_Marketing/KM_Social_Network_Market_Model.R
@@ -0,0 +1,9 @@
+# Reference for data source 
+# Lantz, B. (2013).Finding Groups of Data–Clustering with k-means, In Ashwin Nair & James Jones (Eds.), Machine Learning with R(pp. 285-310). Packt Publishing Ltd.
+
+# K-Means clustering 
+
+# # Collecting data
+SNSData <- read.csv("SNSData.csv", header= TRUE)
+
+str(SNSData)
