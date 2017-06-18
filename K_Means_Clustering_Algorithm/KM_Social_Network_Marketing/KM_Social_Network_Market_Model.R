2bd878c4ee88fe6bf37d9c5b30c0d0dd8fe49172
AI1988 <aijpathirana@gmail.com>
2017-06-17 19:23:18 -0500

    Imputing the missing values

--- K_Means_Clustering_Algorithm/KM_Social_Network_Marketing/KM_Social_Network_Market_Model.R
+++ K_Means_Clustering_Algorithm/KM_Social_Network_Marketing/KM_Social_Network_Market_Model.R
@@ -6,4 +6,35 @@
 # # Collecting data
 SNSData <- read.csv("SNSData.csv", header= TRUE)
 
+# Explore the data
+
 str(SNSData)
+table(SNSData$gender, useNA = "ifany")
+
+
+# Identify missing values using graphical view. See the Rplot.pdf and red colour stripes indicate the missing values.  
+# library(Amelia)
+# missmap(SNSData, main="Missing Data - SNS", col=c("red","grey"), legend=FALSE)
+
+
+
+# Data preparation - imputing 
+# Use age range: If the age is at least 13 and less than 20 years
+SNSData$age <- ifelse(SNSData$age >= 13 & SNSData$age < 20, SNSData$age, NA)
+summary(SNSData$age)
+
+# remove the missing values before calculating mean
+mean(SNSData$age, na.rm = TRUE)
+
+# computes statistics for subgroups of data
+aggregate(data = SNSData, age ~ gradyear, mean, na.rm = TRUE)
+
+# get groups of mean as a vector form
+ave_age <- ave(SNSData$age, SNSData$gradyear, FUN = function(x) mean(x, na.rm = TRUE))
+length(ave_age)
+
+SNSData$age <- ifelse(is.na(SNSData$age), ave_age, SNSData$age)
+summary(SNSData$age)
+
+
+
