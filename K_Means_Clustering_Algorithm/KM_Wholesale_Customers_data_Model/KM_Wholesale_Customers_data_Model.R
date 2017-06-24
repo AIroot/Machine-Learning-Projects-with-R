8b63e0bc1ed86a89e01233df49848265b9e17aa3
AI1988 <aijpathirana@gmail.com>
2017-06-23 20:14:44 -0500

    Identify outliers

--- K_Means_Clustering_Algorithm/KM_Wholesale_Customers_data_Model/KM_Wholesale_Customers_data_Model.R
+++ K_Means_Clustering_Algorithm/KM_Wholesale_Customers_data_Model/KM_Wholesale_Customers_data_Model.R
@@ -19,3 +19,17 @@ str(WholesaleData)
 # #Identify missing values using graphical view. See the Rplot.pdf and red colour stripes indicate the missing values.  
 # library(Amelia)
 # missmap(WholesaleData, main="Missing Data - Wholesale", col=c("red","grey"), legend=FALSE)
+
+
+
+# Identify Outliers in the data set; See Rplots.pdf for visualisation  
+# Create boxplots for continuous variables
+# par(mfrow=c(2,3))
+
+boxplot(WholesaleData$Fresh~WholesaleData$Channel, main=" Fresh",ylab="Fresh",xlab="Channel")
+boxplot(WholesaleData$Milk~WholesaleData$Channel, main=" Milk",ylab="Milk",xlab="Channel")
+boxplot(WholesaleData$Grocery~WholesaleData$Channel, main=" Grocery",ylab="Grocery",xlab="Channel")
+boxplot(WholesaleData$Frozen~WholesaleData$Channel, main="Frozen",ylab="Frozen",xlab="Channel")
+boxplot(WholesaleData$Detergents_Paper~WholesaleData$Channel, main="Detergents_Paper",ylab="Detergents_Paper",xlab="Channel")
+boxplot(WholesaleData$Delicassen~WholesaleData$Channel, main="Delicassen",ylab="Delicassen",xlab="Channel")
+
